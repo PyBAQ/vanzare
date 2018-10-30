02cc8ba5313a3593043581f9a3c41e288bfb13aa
Cesar Augusto <caherdenez@gmail.com>
2018-10-29 19:28:03 -0500

    add test productos

--- vanzare/producto/tests.py
+++ vanzare/producto/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from .models import ProductoBase, Producto
 # Create your tests here.
+
+
+class ProductoBaseTestCase(TestCase):
+
+    fixtures = ['cliente.test.json', 'producto.test.json']
+
+    def test_producto_base_crear(self):
+        ProductoBase.objects.create(
+            nombre='Producto test',
+            valor = 100,
+            valor_cantidad = 1,
+            opciones_cobro = ProductoBase.PRECIO_ESTANDAR
+        )
+        self.assertEqual(ProductoBase.objects.count(), 2)
+
+    def test_producto_crear(self):
+        Producto.objects.create(
+            recibido_id=1,
+            producto_base_id=1,
+            cantidad=1,
+        )
+        self.assertEqual(Producto.objects.count(),2)
